93de3058e44b87e89c1fa4ec0a9dc5506c756f19
Maruthi R <maruthi.syn.ack@gmail.com>
2022-06-25 12:52:14 +0530

    Added Flutter code

---
+++ blockcast/android/app/src/main/kotlin/com/example/blockcast/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blockcast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ blockcast/lib/Homepage.dart
@@ -0,0 +1,308 @@
+        import 'package:flutter/material.dart';
+        import 'dart:math' as math;
+
+import 'package:flutter_svg/parser.dart';
+import 'package:flutter_svg/svg.dart';
+        
+        class HomepageWidget extends StatefulWidget {
+        @override
+        _HomepageWidgetState createState() => _HomepageWidgetState();
+        }
+        
+        class _HomepageWidgetState extends State<HomepageWidget> {
+        @override
+        Widget build(BuildContext context) {
+        // Figma Flutter Generator HomepageWidget - FRAME
+        
+        return Container(
+      width: 428,
+      height: 926,
+      decoration: BoxDecoration(
+          color : Color.fromRGBO(0, 0, 0, 0.7400000095367432),
+  ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+        top: 37,
+        left: 29,
+        child: Transform.rotate(
+        angle: -1.987846675914698e-16 * (math.pi / 180),
+        child: Container(
+        width: 370,
+        height: 62,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(37),
+            topRight: Radius.circular(37),
+            bottomLeft: Radius.circular(37),
+            bottomRight: Radius.circular(37),
+          ),
+      boxShadow : [BoxShadow(
+          color: Color.fromRGBO(0, 0, 0, 0.25),
+          offset: Offset(0,4),
+          blurRadius: 4
+      )],
+      border : Border.all(
+          color: Color.fromRGBO(0, 0, 0, 1),
+          width: 0.20000000298023224,
+        ),
+  )
+      ),
+      )
+      ),Positioned(
+        top: 55,
+        left: 326,
+        child: SvgPicture.asset(
+        'assets/images/vector.svg',
+        semanticsLabel: 'vector'
+      ),
+      ),Positioned(
+        top: 52,
+        left: 67,
+        child: Text('Serch Podcast...', textAlign: TextAlign.center, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 0.6000000238418579),
+        fontFamily: 'Roboto Medium',
+        fontSize: 24,
+        letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+        fontWeight: FontWeight.normal,
+        height: 1
+      ),)
+      ),Positioned(
+        top: 173,
+        left: 53,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(11),
+            topRight: Radius.circular(11),
+            bottomLeft: Radius.circular(11),
+            bottomRight: Radius.circular(11),
+          ),
+      color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 546,
+        left: 53,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(11),
+            topRight: Radius.circular(11),
+            bottomLeft: Radius.circular(11),
+            bottomRight: Radius.circular(11),
+          ),
+      color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 363,
+        left: 53,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(11),
+            topRight: Radius.circular(11),
+            bottomLeft: Radius.circular(11),
+            bottomRight: Radius.circular(11),
+          ),
+      color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 736,
+        left: 53,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(11),
+            topRight: Radius.circular(11),
+            bottomLeft: Radius.circular(11),
+            bottomRight: Radius.circular(11),
+          ),
+      color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 363,
+        left: 257,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(11),
+            topRight: Radius.circular(11),
+            bottomLeft: Radius.circular(11),
+            bottomRight: Radius.circular(11),
+          ),
+      color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 736,
+        left: 257,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(11),
+            topRight: Radius.circular(11),
+            bottomLeft: Radius.circular(11),
+            bottomRight: Radius.circular(11),
+          ),
+      color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 173,
+        left: 260,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(11),
+            topRight: Radius.circular(11),
+            bottomLeft: Radius.circular(11),
+            bottomRight: Radius.circular(11),
+          ),
+      color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 546,
+        left: 260,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          borderRadius : BorderRadius.only(
+            topLeft: Radius.circular(11),
+            topRight: Radius.circular(11),
+            bottomLeft: Radius.circular(11),
+            bottomRight: Radius.circular(11),
+          ),
+      color : Color.fromRGBO(217, 217, 217, 1),
+  )
+      )
+      ),Positioned(
+        top: 173,
+        left: 53,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Guidetopodcastartimages1.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 546,
+        left: 53,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Guidetopodcastartimages4.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 363,
+        left: 53,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Guidetopodcastartimages3.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 736,
+        left: 53,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Guidetopodcastartimages5.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 363,
+        left: 257,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Guidetopodcastartimages2.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 736,
+        left: 257,
+        child: Container(
+        width: 133,
+        height: 132,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Guidetopodcastartimages6.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 173,
+        left: 262,
+        child: Container(
+        width: 128,
+        height: 132,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Guidetopodcastartimagesconvey1.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),Positioned(
+        top: 546,
+        left: 262,
+        child: Container(
+        width: 128,
+        height: 132,
+        decoration: BoxDecoration(
+          image : DecorationImage(
+          image: AssetImage('assets/images/Guidetopodcastartimagesconvey2.png'),
+          fit: BoxFit.fitWidth
+      ),
+  )
+      )
+      ),
+        ]
+      )
+    );
+        }
+        }
+        
